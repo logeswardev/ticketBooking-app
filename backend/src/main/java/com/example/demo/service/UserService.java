78328a3b5c8fc7d64b078fcf90c60f5a388db0a7
lomadhavan <lomadhavan@ebay.com>
2023-12-25 22:42:57 -0500

    fix for exception

--- backend/src/main/java/com/example/demo/service/UserService.java
+++ backend/src/main/java/com/example/demo/service/UserService.java
@@ -1,16 +1,16 @@
 package com.example.demo.service;
 
+import com.example.demo.exceptions.AppException;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.request.UserRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import javax.ws.rs.NotFoundException;
 import java.util.Objects;
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -35,10 +35,11 @@ public class UserService {
 
     public ResponseEntity<User> userLogin(UserRequest userRequest) {
         String userEmailAddress = userRequest.getEmailId();
-        Optional<User> user = userRepository.findByEmailAddress(userRequest.getEmailId());
+        User user = userRepository.findByEmailAddress(userRequest.getEmailId())
+                .orElseThrow(() -> new AppException("Unknown User", HttpStatus.NOT_FOUND));
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        if(user.isPresent() && userEmailAddress.equalsIgnoreCase(user.get().getEmailAddress()) &&
-                passwordEncoder.matches(userRequest.getPassword(), user.get().getPassword())){
+        if(userEmailAddress.equalsIgnoreCase(user.getEmailAddress()) &&
+                passwordEncoder.matches(userRequest.getPassword(), user.getPassword())){
             return ResponseEntity.ok().build();
         }else{
             return ResponseEntity.badRequest().build();
